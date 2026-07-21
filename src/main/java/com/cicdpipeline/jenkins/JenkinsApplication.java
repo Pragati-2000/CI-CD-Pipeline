348ecdbc4381bb0b8697dac3e795cf2a14745d30
Pragati Acharya <pragatiacharya@Pragatis-MacBook-Air.local>
2026-07-22 01:11:36 +0530

    Its my first commit via IDE

--- src/main/java/com/cicdpipeline/jenkins/JenkinsApplication.java
+++ src/main/java/com/cicdpipeline/jenkins/JenkinsApplication.java
@@ -1,5 +1,6 @@
 package com.cicdpipeline.jenkins;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class JenkinsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 		System.out.println("JenkinsApplication started");
+		System.out.println("now i will commit from IDE");
 	}
 
 }
