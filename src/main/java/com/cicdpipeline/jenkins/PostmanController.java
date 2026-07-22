ef01ec4fdb2b5bf4ae639fa22b047aa0fe3e6333
Pragati Acharya <pragatiacharya@Pragatis-MacBook-Air.local>
2026-07-22 18:55:22 +0530

    Added PostmanController

---
+++ src/main/java/com/cicdpipeline/jenkins/PostmanController.java
@@ -0,0 +1,13 @@
+package com.cicdpipeline.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PostmanController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Postman";
+    }
+}
